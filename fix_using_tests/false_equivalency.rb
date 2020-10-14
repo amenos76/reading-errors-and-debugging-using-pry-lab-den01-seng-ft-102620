6d34a5505cc521053535970f84062f2d56bc3090
Augie Menos <augie.menos@gmail.com>
2020-10-14 18:38:10 +0000

    Automatically backed up by Learn

--- fix_using_tests/false_equivalency.rb
+++ fix_using_tests/false_equivalency.rb
@@ -8,13 +8,14 @@ def prompt_user
   puts "What would you like to do?"
   puts "1.) Eat a hamburger."
   puts "2.) Eat a ham."
-  binding.pry
 end
 
 def selection(num)
   if num = 1
+    binding.pry
      "YUM YUM MUNCH MUNCH MUNCH"
   elsif num = 2
+    binding.pry
      "HAM HAM HAM IN MY TUMMY"
   end
 end
